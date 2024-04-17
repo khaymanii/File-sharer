d9a35ac8bb61f9c1bae2866ab57c4fcf46b3cb3a
khaymanii <kennethakpo61@gmail.com>
2024-04-17 14:23:34 +0100

    chore clean up

--- app/(dashboard)/(routes)/files/page.jsx
+++ app/(dashboard)/(routes)/files/page.jsx
@@ -1,7 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 function Files() {
-  return <div>Files</div>;
+  return (
+    <div>
+      Files
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
 
 export default Files;
--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -64,7 +64,7 @@ function Header() {
             <div className="sm:flex sm:gap-4">
               <a
                 className="block rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white transition hover:bg-blue-700"
-                href="#"
+                href="/files"
               >
                 Get Started
               </a>
